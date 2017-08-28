c94c6340e9e450f4f7bf56142d028d2ec22f5a6f
KatOkonska <kat.okonska@gmail.com>
2017-08-29 01:16:19 +0200

    Dodanie plikow DeleteUserType i delete_user.html.twig

---
+++ src/Form/DeleteUserType.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kasia
+ * Date: 19.08.17
+ * Time: 15:08
+ */
+
+
+namespace Form;
+
+use Symfony\Component\Form\AbstractType;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class DeleteUserType
+ *
+ * @package Form
+ */
+class DeleteUserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+
+        $builder->add(
+            'User_password', HiddenType::class, array()
+        );
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'edit_user_type';
+    }
+}
